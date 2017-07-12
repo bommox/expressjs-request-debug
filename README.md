68893a74ed0d9da4be1472fa2334d53e37a84aa2
sixertoy <sixertoy.github@gmail.com>
2017-07-12 10:18:46 +0200

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# Express Simple Logger Middleware
+
+## Install
+
+```bash
+npm i --save expressjs-simple-logger
+```
+
+## Usage
+
+```javascript
+const express = require('express');
+const simplelogger = require('expressjs-simple-logger');
+const app = express();
+app.use(simplelogger());
+```
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const connect = require('connect');
 
+const now = () => {
+  let result = '';
+  let splitter = '/';
+  const space = ' ';
+  const date = new Date();
+  const year = date.getFullYear();
+  const day = date.getDate() < 10
+    ? `0${date.getDate()}`
+    : date.getDate();
+  const month = date.getMonth() < 9
+    ? `0${date.getMonth() + 1}`
+    : date.getMonth() + 1;
+  result = `${year}${splitter}${month}${splitter}${day}`;
+  const hours = date.getHours() < 10
+    ? `0${date.getHours()}`
+    : date.getHours();
+  const minutes = date.getMinutes() < 10
+    ? `0${date.getMinutes()}`
+    : date.getMinutes();
+  const seconds = date.getSeconds() < 10
+    ? `0${date.getSeconds()}`
+    : date.getSeconds();
+  splitter = ':';
+  return `${result}${space}${hours}${splitter}${minutes}${splitter}${seconds}`;
+};
+
 const simplelogger = () => {
   const chain = connect();
   chain.use((req, res, next) => {
-    console.log('req', req);
     const msg = `> Start request at ${utils.now(true)}`;
     console.log(`\x1b[33m${msg}\x1b[39m`);
     console.log(`\x1b[90m${req.headers.origin} -> ${req.method} ${req.url}\x1b[39m`);
